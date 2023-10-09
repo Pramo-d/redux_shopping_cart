74072e88bacf409bea023718ab913ba22feb23a9
Pramo-d <promody1444@gmail.com>
2023-10-09 10:44:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
-  const showCart=useSelector(state=>state.ui.cartIsAvailable)
+  const showCart=useSelector(state=>state.ui.cartIsAvailable);
+  const cart =useSelector(state=>state.cart);
+
+  useEffect(()=>{
+   fetch('https://redux-cart-41542-default-rtdb.firebaseio.com/cart.json',{
+    method:"PUT",
+    body:JSON.stringify(cart),
+    headers:{
+      'Content-Type':'application/json'
+    }
+   })
+  },[cart]);
   return (
     <Layout>
     {showCart &&  <Cart />}
